44f3f0be3437a0c3bac47e99c5dc2044a39d28bc
Meslim <meslimawad@gmail.com>
2025-06-11 22:28:55 +0300

    finished 2ex

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,14 @@
+void    ft_ft(int *nbr)
+{
+    *nbr = 42;
+}
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     int x;
+//     x = 0;
+//     ft_ft(&x);
+//     printf("%d", x);
+//     return 0;
+// }
